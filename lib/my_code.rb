ee3d83227dc0065babf36b0d6a87edf36844fd54
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-06-11 22:06:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(source_array)
+ new_array = []
+ source_array.length.times do |index|
+   new_array.push(yield(source_array[index]))
+ end
+ return new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  running_total = starting_point
+  source_array.length.times do |index|
+    running_total = yield(source_array[index], running_total)
+    print running_total
+  end
+  if(running_total == nil)
+    running_total = false
+  end
+  return running_total
+end
