460986e527f13b78743a48f6c19a4a8fcbd2761f
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-06-29 04:12:03 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,21 +1,21 @@
 # Your Code Here
 def map(arr)
- new_arr=[] 
+  new_arr=[] 
   for i in arr
-   new_arr.push(yield(i))
+    new_arr.push(yield(i))
   end 
-   return new_arr
+  return new_arr
 end
 
 def reduce(arr, start=0)
   total=start
   for i in arr
- total=yield(total, i)
- end
- if total==false
-  return false
-elsif total==0 || total==true
-return true
-end
+    total=yield(total, i)
+  end
+  if total==false
+    return false
+  elsif total==0 || total==true
+    return true
+  end
   return total
 end
