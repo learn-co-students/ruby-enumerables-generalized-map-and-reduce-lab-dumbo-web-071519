5543a6ddc441f7394accdcebf06b9a6320e68193
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-06 21:43:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,27 @@
 # Your Code Here
-def my_own_map(source_array)
-    answer = source_array.map { |n| n > 0 ? -n : -n }
-  return answer
-endd 
+def map(s)
+  new = []
+  i = 0
+  while i < s.length
+    new.push(yield(s[i]))
+    i += 1
+  end
+  new
+end
+
+
+
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
